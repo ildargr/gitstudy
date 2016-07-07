33f30ef867dddaf431fce988d52dd4e58993d870
ildafon <ildafon@yandex.ru>
2016-07-07 14:53:07 +0300

    users modified

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('SORRY, USERS NOT FOUND');
+  //res.send('SORRY, USERS NOT FOUND');
+  res.render('users',{
+    user_title:"Пользователи",
+    users_count:"0"
+  });
 });
 
 module.exports = router;
