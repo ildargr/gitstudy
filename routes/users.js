fb8f0ef2c7d1b8bdeac1bbb483204b57ba11affd
ildafon <ildafon@yandex.ru>
2016-07-07 10:16:40 +0300

    iss2 solved

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('USERS NOT FOUND');
+  res.send('SORRY, USERS NOT FOUND');
 });
 
 module.exports = router;
